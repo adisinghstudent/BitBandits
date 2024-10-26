c12fedac6d7139c42d6312cc66a4094896a88354
IvanMordovets <imordovets@gmail.com>
2024-10-26 13:42:33 +0200

    updated path for optimize battery schedule

--- Case2/visualize.py
+++ Case2/visualize.py
@@ -2,7 +2,7 @@
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 
-from BitBandits.Case2.optimize_battery_schedule import optimize_battery_schedule
+from optimize_battery_schedule import optimize_battery_schedule
 
 def plot_result(spot_prices, load, pv_production, grid_power, soc):
     # Create time array for x-axis
